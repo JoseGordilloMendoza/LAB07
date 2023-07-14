848ff6bc50f8c8452fa70e2fec82b873e661655b
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-07-14 02:21:34 -0500

    Creando la vista para el envio de email, con asunto, contenido, remitente y destinario

--- lab07PWEB/lab07emails_pdf/emails/send/views.py
+++ lab07PWEB/lab07emails_pdf/emails/send/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+def index(request):
+    send_mail('EMAIL DESDE DJANGO', # Asunto
+              'Probando envio de email desde DJANGO, hola :D', # Contenido del mensaje
+              'sjbalonzo201c@gmail.com', # Remitente
+              ['jgordillome@unsa.edu.pe'], # Destinario
+              fail_silently=False)
+    return render(request, 'send/index.html')
+
